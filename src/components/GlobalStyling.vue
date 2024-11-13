3c18f0269abc957e7aab94bbe690614b0c1aad78
ravin yadav <ravinyadav973@gmail.com>
2024-11-13 17:56:52 +0530

    global vs local styling

---
+++ src/components/GlobalStyling.vue
@@ -0,0 +1,25 @@
+
+
+<template>
+    <h1 class="text-red">Global Styling</h1>
+    <p class="local">Tying local scope in this component</p>
+</template>
+
+
+
+<!-- global style -->
+
+<!-- <style>
+.text-red { 
+  color: red;  
+}
+</style> -->
+
+
+<!-- local style -->
+
+<style scoped>
+.text-red { 
+  color: red;  
+}
+</style>
---
+++ src/components/LocalStyling.vue
@@ -0,0 +1,6 @@
+
+
+
+<template>
+    <h2 class="text-red">This is children component</h2>
+</template>
