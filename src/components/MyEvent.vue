fba78fde8afbee3252b8dbb44026e19683790ce4
ravin yadav <ravinyadav973@gmail.com>
2024-11-13 18:34:01 +0530

    event hadler with seperate function

--- src/components/MyEvent.vue
+++ src/components/MyEvent.vue
@@ -3,13 +3,24 @@
 <script setup>
 import {ref} from "vue"
 let count = ref(0);
+
+
+const incrementCounter = () => {
+    count.value++ //value comming from ref
+}
+
+const decrementCounter = () => {
+    count.value-- 
+}
 </script>
 
 
 <template>
     <h1>count: {{ count }}</h1>
 
-    <button v-on:click="count++">increase</button>
-    <button @:click="count++">increase</button>
+    <!-- <button v-on:click="count++">increase</button>
+    <button @:click="count++">increase</button> -->
+    <button @:click="incrementCounter">increase</button>
+    <button @:click="decrementCounter">increase</button>
 
 </template>
